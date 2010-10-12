95bdab0abda4b8f61a60a8fabcce57807e5d835d
Victor Cassen <phonybone@gmail.com>
2010-10-11 17:03:17 -0700

    migrating to external event handlers; not finished

--- src/org/phonybone/sprites/Game.java
+++ src/org/phonybone/sprites/Game.java
@@ -101,11 +101,9 @@ public class Game extends Canvas {
 	    });
 		
 	// add a key input system (defined below) to our canvas
-	// so we can respond to key pressed
-	addKeyListener(new KeyInputHandler());
-		
-	// Also mouse activity:
-	addMouseListener(new MouseInputHandler());
+	// so we can respond to key pressed (and mouse events, too.)
+	addKeyListener(new GameKeyHandler());
+	addMouseListener(new GameMouseHandler());
 
 	// request the focus so key events come to us
 	requestFocus();
@@ -305,120 +303,6 @@ public class Game extends Canvas {
      * 
      * @author Kevin Glass
      */
-    private class KeyInputHandler extends KeyAdapter {
-	/** The number of key presses we've had while waiting for an "any key" press */
-	private int pressCount = 1;
-	
-	/**
-	 * Notification from AWT that a key has been pressed. Note that
-	 * a key being pressed is equal to being pushed down but *NOT*
-	 * released. Thats where keyTyped() comes in.
-	 *
-	 * @param e The details of the key that was pressed 
-	 */
-	public void keyPressed(KeyEvent e) {
-	    System.err.println("KeyPressed event: "+e.toString());
-	    // if we're waiting for an "any key" typed then we don't 
-	    // want to do anything with just a "press"
-	    if (waitingForKeyPress) {
-		waitingForKeyPress=false;
-		System.err.println("any key pressed");
-		return;
-	    }
-			
-			
-	    if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-		leftPressed = true;
-	    }
-	    if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-		rightPressed = true;
-	    }
-	    if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-		firePressed = true;
-	    }
-	} 
-		
-	/**
-	 * Notification from AWT that a key has been released.
-	 *
-	 * @param e The details of the key that was released 
-	 */
-	public void keyReleased(KeyEvent e) {
-	    // if we're waiting for an "any key" typed then we don't 
-	    // want to do anything with just a "released"
-
-	    System.err.println("KeyReleased event: "+e.toString());
-
-	    if (waitingForKeyPress) {
-		waitingForKeyPress=false;
-		System.err.println("any key released");
-		return;
-	    }
-			
-	    if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-		leftPressed = false;
-	    }
-	    if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-		rightPressed = false;
-	    }
-	    if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-		firePressed = false;
-	    }
-	}
-
-	/**
-	 * Notification from AWT that a key has been typed. Note that
-	 * typing a key means to both press and then release it.
-	 *
-	 * @param e The details of the key that was typed. 
-	 */
-	public void keyTyped(KeyEvent e) {
-	    // if we're waiting for a "any key" type then
-	    // check if we've recieved any recently. We may
-	    // have had a keyType() event from the user releasing
-	    // the shoot or move keys, hence the use of the "pressCount"
-	    // counter.
-	    
-	    System.err.println("KeyTyped event: "+e.toString());
-	    
-	    if (waitingForKeyPress) {
-		if (pressCount == 1) {
-		    // since we've now recieved our key typed
-		    // event we can mark it as such and start 
-		    // our new game
-		    waitingForKeyPress=false;
-		    System.err.println("any key released");
-		    startGame();
-		    pressCount = 0;
-		} else {
-		    pressCount++;
-		}
-	    }
-			
-	    switch (e.getKeyChar()) {
-	    case ' ': 
-		gameRunning=!gameRunning;
-		break;
-	    case 'q':
-	    case 'Q':
-	    case 27:		// escape (in theory)
-		gameOver=true;
-		break;
-	    }
-
-	}
-    }
-
-    private class MouseInputHandler extends MouseAdapter {
-	public void mousePressed(MouseEvent e) {
-	}
-	public void mouseReleased(MouseEvent e) {
-	}
-	public void mouseEntered(MouseEvent e) {
-	}
-	public void mouseExited(MouseEvent e) {
-	}
-    }
 
     /**
      * The entry point into the game. We'll simply create an
